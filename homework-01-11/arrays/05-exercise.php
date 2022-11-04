4668fc0eec9954a398d319a092cb02862288a858
Felikss_Vasiljevs <112703289+felix-vasiljevs@users.noreply.github.com>
2022-11-04 17:08:28 +0200

    Add files via upload

--- homework-01-11/arrays/05-exercise.php
+++ homework-01-11/arrays/05-exercise.php
@@ -1,38 +1,93 @@
 <?php
 
-function display_board($playGround)
+$playGround = [
+    [' ', ' ', ' '],  //  0
+    [' ', ' ', ' '],  //  1
+    [' ', ' ', ' ']   //  2
+//        0    1    2
+];
+
+$combinations = [
+//  horizontal
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+//  vertical
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+//  diagonal
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
+];
+
+function displayBoard(array $board)
 {
-    $playGround = [
-        ['1', '-', '-'],  //  0
-        ['-', '-', '-'],  //  1
-        ['-', '-', '-']   //  2
-//        1    2    3
-    ];
-
-    foreach ($playGround as $key => $value) {
-        echo implode("| ", $value) . "\n";
-//       echo "{$key} => {$value}";
-//       print_r($playGround);
-    }
-
-    $row = $key;
-
-    $playerX = "X";
-
-    $playGround []= $playerX;
-
-//    return $playGround. "\n";
-//    $playGround[1][2] = "O";
-
-//    if ()
-
-//    echo "   |   |   \n";
-//    echo "---+---+---\n";
-//    echo "   |   |   \n";
-//    echo "---+---+---\n";
-//    echo "   |   |   \n";
+    echo " {$board[0][0]} | {$board[0][1]} | {$board[0][2]}  \n";
+    echo "---+---+---\n";
+    echo " {$board[1][0]} | {$board[1][1]} | {$board[1][2]}  \n";
+    echo "---+---+---\n";
+    echo " {$board[2][0]} | {$board[2][1]} | {$board[2][2]}  \n";
 }
 
-display_board(1, 0);
+echo "Let the game begin!\n";
 
+displayBoard($playGround);
 
+$playerA = 'X';
+$playerB = 'O';
+$turns = 0;
+
+$currentPlayer = $playerA;
+
+while (true)
+{
+    $playerChoice = readline("\n '{$currentPlayer}', choose your location (row, column).\n");
+    [$x, $y] = explode(' ', $playerChoice);
+
+    if ($x == null || $y == null) {
+        displayBoard($playGround);
+        echo "\nInvalid input. Enter your location with numbers with spaces. (0 2)\n";
+        continue;
+    }
+
+    if ($playGround[$x][$y]  == ' '){
+        $playGround[$x][$y] = $currentPlayer;
+    } else {
+        displayBoard($playGround);
+        echo "\nThis cell is already filled. Please, choose another cell.\n";
+        continue;
+    }
+
+    $turns++;
+
+    echo PHP_EOL;
+    displayBoard($playGround);
+
+    foreach ($combinations as $combination) {
+        $conditionCounter = 0;
+
+        foreach ($combination as $position) {
+            [$x, $y] = $position;
+            if ($playGround[$x][$y] !== $currentPlayer) {
+                break;
+            }
+            $conditionCounter++;
+        }
+
+        if ($conditionCounter == 3) {
+            echo "\n Player {$currentPlayer} wins!\n";
+            exit;
+        }
+    }
+
+//    Game is a tie, exit the game
+    if ($turns === 9) {
+        echo "Tie!";
+        exit;
+    }
+
+//    Change player turns
+    $currentPlayer = $currentPlayer === $playerA ? $playerB : $playerA;
+}
+?>
