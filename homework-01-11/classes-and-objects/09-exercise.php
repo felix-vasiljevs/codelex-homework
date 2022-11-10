601faba087d5693f965429a7cff36f2edea8314d
Felikss_Vasiljevs <112703289+felix-vasiljevs@users.noreply.github.com>
2022-11-11 00:03:50 +0200

    Add files via upload

--- homework-01-11/classes-and-objects/09-exercise.php
+++ homework-01-11/classes-and-objects/09-exercise.php
@@ -3,16 +3,30 @@
 class BankAccount
 {
     private string $user;
-    private int $balance;
+    private float $balance;
 
-    public function __construct(string $user, int $balance)
+    public function __construct(string $user, float $balance)
     {
         $this->user = $user;
         $this->balance = $balance;
     }
 
-    function show_user_name_and_balance(string $user)
+    public function getBalance(): float
     {
+        return abs($this->balance);
+    }
 
+    public function show_user_name_and_balance(): string
+    {
+        if ($this->balance < 0) {
+            return $this->user . ', ' . '-' . '$' . number_format($this->getBalance(), 2);
+        }
+        return $this->user . ', ' . '$' . number_format($this->balance, 2);
     }
 }
+
+$bensonAccount = new BankAccount('Benson', -17.50);
+
+echo $bensonAccount->show_user_name_and_balance() . PHP_EOL;
+
+
